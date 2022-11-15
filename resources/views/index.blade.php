151aec94a922703e57c681681edc910f367a958a
Felipe Massola <massolafelipe@gmail.com>
2022-11-15 17:49:06 -0300

    Primeiro commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,26 +2,25 @@
 
 @section('conteudo')
     <section class="landing-page">
-            <div class="container-land">
-                <div class="container-slogan">
-                    <p class="slogan">Entre agora no mundo da <highlight>energia solar</highlight> como nunca antes!</p>
+        <div class="container-land">
+            <div class="container-slogan">
+                <p class="slogan">Entre agora no mundo da <b class="highlight">energia solar</b> como nunca antes!</p>
+            </div>
+            <div class="container-box">
+                <div id="title-box">
+                    Cadastre-se e realize uma solicitação quando quiser!
                 </div>
-                <div class="container-box">
-                    <div id="title-box">
-                        Cadastre-se e realize uma solicitação quando quiser!
-                    </div>
-                    <form>
-                        <div class="input-label">Email</div>
-                        <input type="email" placeholder="exemplo@email.com" name="email">
+                <form>
+                    <div class="input-label">Email</div>
+                    <input type="email" placeholder="exemplo@email.com" name="email">
 
-                        <div class="input-label">Senha</div>
-                        <input type="password" placeholder="•••••••••••••" name="password">
+                    <div class="input-label">Senha</div>
+                    <input type="password" placeholder="•••••••••••••" name="password">
 
-                        <div class="input-label">Confirmar Senha</div>
-                        <input type="password" placeholder="•••••••••••••" name="verifpassword">
-                    </form>
-                </div>
-            </div>   
-        </figure>
+                    <div class="input-label">Confirmar Senha</div>
+                    <input type="password" placeholder="•••••••••••••" name="verifpassword">
+                </form>
+            </div>
+        </div>   
     </section>
 @endsection
